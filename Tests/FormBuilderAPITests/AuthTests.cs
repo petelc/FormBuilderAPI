9657f602d16893edd0dafc6573ffe66b3a89abb7
Peter Carroll <petelc1966@hotmail.com>
2025-08-23 04:23:02 -0400

    Working on Tests

---
+++ Tests/FormBuilderAPITests/AuthTests.cs
@@ -0,0 +1,32 @@
+﻿using FormBuilderAPI.Controllers;
+using Xunit.Abstractions;
+
+namespace FormBuilderAPITests;
+
+public class AuthTests : IDisposable
+{
+    private AccountController  _sut;
+    private readonly ITestOutputHelper _output;
+
+    public AuthTests(ITestOutputHelper output)
+    {
+        
+        _output = output;
+        _output.WriteLine("In the constructor of AuthTests class");
+    }
+    
+    [Fact]
+    public void UserShouldBeAbleToLogin()
+    {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
